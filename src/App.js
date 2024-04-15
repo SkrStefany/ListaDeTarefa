da967e89ca56e38bd514c3cf3ba154ed7bfd93ee
Stefany Souza <sttefanyss14@gmail.com>
2024-04-15 19:37:02 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'; 
+
 
 function App() {
+  const [input, setInput] = useState('');
+  
+  const [tarefas, setTarefas] = useState([
+    
+  ]);
+
+  const removerTarefa = (index) => {
+    const novasTarefas = [...tarefas];
+    novasTarefas.splice(index, 1);
+    setTarefas(novasTarefas);
+  };
+  
+
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('@tarefas');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefas', JSON.stringify(tarefas));
+  }, [tarefas]);
+
+
+
+  function enviar(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input])
+    setInput('');
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class='container'>
+      <h1 class='Titulo'>Lista de Tarefas </h1>
+
+      <form onSubmit={enviar}>
+        <label>Tarefa: </label> <br/>
+        <input placeholder='Digite aqui:' value={input} onChange={(e) => setInput(e.target.value)}/>
+        <button type='submit' >Enviar</button>
+      </form>
+
+    
+  <ul>
+    {tarefas.map((tarefa, index) => (
+      <li key={index}>
+        {tarefa}
+        <button class='remover' onClick={() => removerTarefa(index)}>Remover</button>
+      </li>
+    ))}
+  </ul>
+
+      
     </div>
   );
 }
